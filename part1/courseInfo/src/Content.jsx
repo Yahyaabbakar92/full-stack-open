7a3afcddafb5f25cf2e8b34042087de984281609
Yahya <yahyaabbakar@gmail.com>
2024-01-11 06:48:05 +0200

    added object parts and refactored components

--- part1/courseInfo/src/Content.jsx
+++ part1/courseInfo/src/Content.jsx
@@ -1,11 +1,11 @@
 import Part from './Part'
 
-const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+const Content = ({ name1, exercises1, name2, exercises2, name3, exercises3 }) => {
   return (
     <>
-    <Part part={part1} exercises={exercises1} />
-    <Part part={part2} exercises={exercises2} />
-    <Part part={part3} exercises={exercises3} />
+    <Part name={name1} exercises={exercises1} />
+    <Part name={name2} exercises={exercises2} />
+    <Part name={name3} exercises={exercises3} />
     </>
   )
 }
