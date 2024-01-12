0bfaf2c01534e3d7738baa712e8cfe5a752d0b3c
Yahya <yahyaabbakar@gmail.com>
2024-01-12 08:19:13 +0200

    added Header component

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './Header'
 
 const App = () => {
   // save clicks of each button to its own state
@@ -8,7 +9,16 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header />
+
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
