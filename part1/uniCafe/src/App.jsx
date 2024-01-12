7b1b663e888dacbdd5bd2521747bcbe83ce2bd05
Yahya <yahyaabbakar@gmail.com>
2024-01-12 08:24:40 +0200

    added click functionality to buttons

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -7,13 +7,24 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+
   return (
     <div>
       <Header />
 
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
 
       <h1>Statistics</h1>
       <p>Good: {good}</p>
