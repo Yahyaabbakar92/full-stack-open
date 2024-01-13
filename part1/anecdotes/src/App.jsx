8f62c96d9627cead5297a3983f30bd1f19680664
Yahya <yahyaabbakar@gmail.com>
2024-01-13 11:16:50 +0200

    added the function to generate a random anecdote

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,12 +11,17 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const handleRandomAnecdote = () => {
+    return setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
 
   return (
     <div>
       {anecdotes[selected]}
+      <br />
+      <button onClick={handleRandomAnecdote}>Next Anecdote</button>
     </div>
   )
 }
