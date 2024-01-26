25edd4dabeeabfe32bfce381bce85479c2d06266
Yahya <yahyaabbakar@gmail.com>
2024-01-26 09:01:53 +0200

    added Person component made it the functionality to add people to the phone book

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,24 +1,42 @@
 import { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
   ]) 
   const [newName, setNewName] = useState('')
+  
+  const addPersons = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
 
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value)
+  }
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
+      <h2>Phone Book</h2>
+      <form onSubmit={addPersons}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => (
+      <Person key={person.name} person={person} />
+        ))}
+      <div>debug: {newName}</div>
     </div>
   )
 }
